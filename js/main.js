c39742d45d5219184c53da19c0aa16b40e7187f6
AgentPuff <jo.reusing@gmail.com>
2017-07-18 15:12:35 -0400

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,37 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function screenAdjust() {
+    var x = document.getElementById("mynav");
+    var y = document.getElementById("mybody");
+
+    if (x.className === "mainnav") {
+        x.className += " responsive";
+        y.className += " responsive";
+    } else {
+        x.className = "mainnav";
+        y.className = "";
+    }
+}
+
+function clickDropdownItem(){
+	var x = document.getElementById("mynav");
+    var y = document.getElementById("mybody");
+    if(x.className == "mainnav responsive"){
+    	x.className = "mainnav";
+        y.className = "";
+    }
+}
+
+
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 500);
+    return false;
+});
+
+
+
+function scrollToAnchor(anchorID){
+    var aTag = $("a[name='"+ anchorID +"']");
+    $('html,body').animate({scrollTop: aTag.offset().top},'slow');
+}
