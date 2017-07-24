720777911624bbdbaf56f90ee03a631857304732
AgentPuff <jo.reusing@gmail.com>
2017-07-24 11:46:58 -0400

    animations

--- js/main.js
+++ js/main.js
@@ -1,15 +1,10 @@
-// Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon
+// Toggle between adding and removing the "show-items" class to topnav when the
+// user clicks on the icon
 function screenAdjust() {
-    var x = document.getElementById("mynav");
-    var y = document.getElementById("mybody");
-
-    if (x.className === "mainnav") {
-        x.className += " responsive";
-        y.className += " responsive";
-    } else {
-        x.className = "mainnav";
-        y.className = "";
-    }
+  var x = document.getElementById("mynav");
+  var y = document.getElementById("mybody");
+  $("#mynav").toggleClass('show-items');
+  $("#mybody").toggleClass('show-items');
 }
 
 // Description: Hides dropdown menu after clicking dropdown menu item
@@ -17,17 +12,20 @@ function screenAdjust() {
 //    Output: Null
 function clickDropdownItem(){
 	var x = document.getElementById("mynav");
-    var y = document.getElementById("mybody");
-    if(x.className == "mainnav responsive"){
-    	x.className = "mainnav";
-        y.className = "";
-    }
+  var y = document.getElementById("mybody");
+  if($('#mynav').hasClass('show-items')){
+    //$(".right-items").toggleClass('hide');
+    //$(".right-items").toggleClass('right-items');
+    //$("#mybody").toggleClass('show-items');
+    $("#mynav").toggleClass('show-items');
+    $("#mybody").toggleClass('show-items');
+  }
 }
 
 // Description: Scrolls page to the anchor specified
 //    Input: Anchor ID or name
 //    Output: Null
 function scrollToAnchor(anchorID){
-    var aTag = $("a[name='"+ anchorID +"']");
-    $('html,body').animate({scrollTop: aTag.offset().top},350,'swing');
+  var aTag = $("a[name='"+ anchorID +"']");
+  $('html,body').animate({scrollTop: aTag.offset().top},350,'swing');
 }
