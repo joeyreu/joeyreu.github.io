e1d0ab7763e287cdda59b2293cf791efa67a4d2c
AgentPuff <jo.reusing@gmail.com>
2017-07-27 11:24:56 -0400

    test changes 2

--- js/main.js
+++ js/main.js
@@ -78,21 +78,77 @@ var c = canvas.getContext('2d');
 // }
 console.log(canvas);
 
-function Circle(x, y){
+// Circle object (capital letter for objects)
+function Circle(x, y, dx, dy, radius){
   this.x = x;
   this.y = y;
+  this.dx = dx;
+  this.dy = dy;
+  this.radius = radius;
+
+  this.draw = function() {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI*2, false);
+    c.strokeStyle='lightblue';
+    c.stroke();
+  }
+
+  this.update = function() {
+    if(this.x+this.radius > innerWidth || this.x-this.radius < 0){
+      this.dx = -this.dx;
+    }
+    if(this.y+this.radius > innerHeight || this.y-this.radius < 0){
+      this.dy = -this.dy;
+    }
+    this.x += this.dx;
+    this.y += this.dy;
+
+    this.draw();
+  }
 
 }
+
+var circleArray = [];
+for(var i = 0; i < 100; i ++){
+  var x = Math.random()*window.innerWidth;
+  var y = Math.random()*window.innerHeight;
+  var dx = (Math.random() - 0.5)*20;
+  var dy = (Math.random() - 0.5)*20;
+  var radius = 30;
+
+  // make sure circle doesn't spawn partially outside canvas
+  while(x+radius > innerWidth || x-radius < 0){
+    x = Math.random()*window.innerWidth;
+  }
+  while(y+radius > innerHeight || y-radius < 0){
+    y = Math.random()*window.innerHeight;
+  }
+
+  circleArray.push(new Circle(x, y, dx, dy, radius))
+}
+
+var circle = new Circle(200, 200, 5, 5, 30);
 
 
 var x = Math.random()*window.innerWidth;
 var y = Math.random()*window.innerHeight;
-var dx = (Math.random() - 0.5)*10;
-var dy = (Math.random() - 0.5)*10;
+var dx = (Math.random() - 0.5)*20;
+var dy = (Math.random() - 0.5)*20;
 var radius = 30;
+
+// make sure circle doesn't spawn partially outside canvas
+while(x+radius > innerWidth || x-radius < 0){
+  x = Math.random()*window.innerWidth;
+}
+while(y+radius > innerHeight || y-radius < 0){
+  y = Math.random()*window.innerHeight;
+}
+
 function animate(){
   requestAnimationFrame(animate);
   c.clearRect(0,0,innerWidth, innerHeight);
+
+
   c.beginPath();
   c.arc(x, y, radius, 0, Math.PI*2, false);
   c.strokeStyle='blue';
@@ -105,6 +161,13 @@ function animate(){
   }
   x += dx;
   y += dy;
+
+  for(var i = 0; i < circleArray.length; i ++){
+    circleArray[i].update();
+  }
+
+  // circle object
+  circle.update();
 }
 animate();
 
